a0228b8672136c9f79a1901c7d82b21bd55cd010
Cezar Neaga <hi@cezarneaga.eu>
2020-08-17 17:40:34 +0200

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -3,11 +3,12 @@ const path = require('path')
 module.exports = {
   siteTitle: `Nu tot ei!`,
   siteDescription:
-    'Ceva descriere: cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+    'Nu tot ei. Pentru că impostura, nepotismul, nesimțirea și corupția sunt coloana vertebrală a clasei noastre politice. Și pentru că putem să îi schimbăm. Pentru că trebuie să o facem. Pentru că avem nevoie de decență, bun simț, meritocrație si oameni care au făcut ceva nu oameni care promit că vor face ceva în politică.',
   copyright: `©${new Date().getFullYear()} Valeriu Nicolae`,
   siteTitleShort: 'NTE',
   social: {
     twitter: 'https://twitter.com/valeriu',
+    email: 'valeriu@nutotei.ro',
     facebook: 'https://facebook.com/valeriu.nicolae.39',
   },
   fbAppId: '123',
