15472b4a47ecaf1590ef9737c0895b3cd33bb223
Bogdan Iasinovschi <biasinovschi@gmail.com>
2020-09-09 20:00:33 +0300

    sectiune text donatii

--- site-config.js
+++ site-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'Nu tot ei. Pentru că impostura, nepotismul, nesimțirea și corupția sunt coloana vertebrală a clasei noastre politice. Și pentru că putem să îi schimbăm. Pentru că trebuie să o facem. Pentru că avem nevoie de decență, bun simț, meritocrație si oameni care au făcut ceva nu oameni care promit că vor face ceva în politică.',
   copyright: `©${new Date().getFullYear()} Valeriu Nicolae`,
   siteTitleShort: 'NTE',
+  donate: 'Dacă doriți să sprijiniți inițiativa #nutotei puteți să donați la Asociația Casa Bună\nCont RON: RO47BTRLRONCRT0566398301\nCont EUR: RO94BTRLEURCRT0566398301',
   social: {
     twitter: 'https://twitter.com/valeriu',
     email: 'valeriu@nutotei.ro',
